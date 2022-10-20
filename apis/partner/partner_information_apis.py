8e9462618a32e9c5c6a3c8f9a6fc793c56231185
liufangjing <fj.liu@teletraan.io>
2022-10-20 14:40:06 +0800

    完成业务伙伴api

--- apis/partner/partner_information_apis.py
+++ apis/partner/partner_information_apis.py
@@ -16,13 +16,66 @@ class PartnerInformation(GetTokenHeader):
         endpoint = HTTPEndpoint(url=self.url, base_headers=self.headers)
         op = Operation(Mutation)
         op.create_partner(input=variables)
-        op.create_partner()
         # start 和 end 的时间戳类型有sgqlc的缺陷，利用字符串替换手动更替
         op1 = str(op).replace('"start"', 'start')
         op2 = str(op1).replace('"end"', 'end')
         data = endpoint(op2)
         try:
             res = (op + data).create_partner
+            return res
+        except:
+            res = data.get("errors")[0].get("message")
+            return res
+
+    def update_partner_api(self, variables):
+        """
+        更新业务伙伴
+        :param variables:
+        :return:
+        """
+        endpoint = HTTPEndpoint(url=self.url, base_headers=self.headers)
+        op = Operation(Mutation)
+        op.update_partner(input=variables)
+        # start 和 end 的时间戳类型有sgqlc的缺陷，利用字符串替换手动更替
+        op1 = str(op).replace('"start"', 'start')
+        op2 = str(op1).replace('"end"', 'end')
+        data = endpoint(op2)
+        try:
+            res = (op + data).update_partner
+            return res
+        except:
+            res = data.get("errors")[0].get("message")
+            return res
+
+    def active_partner_api(self, ids):
+        """
+        激活业务伙伴
+        :param ids: list
+        :return:
+        """
+        endpoint = HTTPEndpoint(url=self.url, base_headers=self.headers)
+        op = Operation(Mutation)
+        op.active_partner(ids=ids)
+        data = endpoint(op)
+        try:
+            res = (op + data).active_partner
+            return res
+        except:
+            res = data.get("errors")[0].get("message")
+            return res
+
+    def frozen_partner_api(self, ids):
+        """
+        冻结业务伙伴
+        :param ids: list
+        :return:
+        """
+        endpoint = HTTPEndpoint(url=self.url, base_headers=self.headers)
+        op = Operation(Mutation)
+        op.frozen_partner(ids=ids)
+        data = endpoint(op)
+        try:
+            res = (op + data).frozen_partner
             return res
         except:
             res = data.get("errors")[0].get("message")
--- case_data/partner_data/partner_information_data.py
+++ case_data/partner_data/partner_information_data.py
@@ -10,7 +10,6 @@ class PartnerInformationData(BaseApi):
 
         args.append(("abbreviation", self.faker.company_prefix()))
         args.append(("companyAddr:address", self.faker.street_address()))
-        args.append(("contactList>0:fixedPhone",self.faker.msisdn()))
         args.append(("contactList>0:phone", self.faker.phone_number()))
         args.append(("contactList>0:name", self.faker.first_name()))
         args.append(("contactList>0:position", self.faker.job()))
@@ -18,20 +17,36 @@ class PartnerInformationData(BaseApi):
         args.append(("creditCode", self.faker.ean13()))
         # args.append(("defaultCurrency:id", ))
         args.append(("licenseCode", self.faker.ean13()))
-
         args.append(("name", self.faker.company()))
         args.append(("no", self.faker.ean8()))
         args.append(("remark", self.faker.paragraph(nb_sentences=3, variable_nb_sentences=True, ext_word_list=None)))
         variables = self.modify_variables(target_json=variables_temp, args=args)
         return variables
 
+    def update_partner_data(self):
+        args = list()
+        variables_temp = self.get_variables(module_name="partner", variables_name="update_partner_data")
+
+        args.append(("abbreviation", self.faker.company_prefix()))
+        args.append(("companyAddr:address", self.faker.street_address()))
+        args.append(("contactList>0:phone", self.faker.phone_number()))
+        args.append(("contactList>0:name", self.faker.first_name()))
+        args.append(("contactList>0:position", self.faker.job()))
+        args.append(("contactList>0:remark", self.faker.ean13()))
+        args.append(("creditCode", self.faker.ean13()))
+        # args.append(("defaultCurrency:id", ))
+        args.append(("licenseCode", self.faker.ean13()))
+        args.append(("name", self.faker.company()))
+        args.append(("remark", self.faker.paragraph(nb_sentences=3, variable_nb_sentences=True, ext_word_list=None)))
+        variables = self.modify_variables(target_json=variables_temp, args=args)
+        return variables
+
 
 if __name__ == '__main__':
-    p=PartnerInformation()
+    p = PartnerInformation()
     for i in range(100):
         pd = PartnerInformationData()
-        data=pd.create_partner_data()
+        data = pd.create_partner_data()
 
-        res=p.create_partner_api(data)
+        res = p.create_partner_api(data)
         print(res)
-
